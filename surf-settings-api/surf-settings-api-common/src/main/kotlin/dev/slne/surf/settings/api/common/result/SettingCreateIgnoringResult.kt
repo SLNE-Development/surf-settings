4f6b2a001eb2b60bd5e3b235d9b534b007121c11
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-10-10 16:39:10 +0200

    refactor: rename SettingModifyResult to SettingCreateIgnoringResult and update related types

---
+++ surf-settings-api/surf-settings-api-common/src/main/kotlin/dev/slne/surf/settings/api/common/result/SettingCreateIgnoringResult.kt
@@ -0,0 +1,15 @@
+package dev.slne.surf.settings.api.common.result
+
+import dev.slne.surf.settings.api.common.setting.Setting
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class SettingCreateIgnoringResult {
+    data class Success(val setting: Setting) : SettingCreateIgnoringResult()
+    data class Failure(val failureReason: SettingCreateIgnoringFailureReason) :
+        SettingCreateIgnoringResult()
+
+    enum class SettingCreateIgnoringFailureReason {
+        INTERNAL_ERROR
+    }
+}
