8d123f8a5710ffc3679bdd47103ee0a3d62ed2f9
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-10-13 15:47:52 +0200

    feat: add surf-settings-core-client module to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,9 @@ rootProject.name = "surf-settings"
 
 include("surf-settings-api:surf-settings-api-common")
 include("surf-settings-core:surf-settings-core-common")
+include("surf-settings-core:surf-settings-core-client")
+
 include("surf-settings-paper")
 include("surf-settings-velocity")
 include("surf-settings-server")
-include("surf-settings-core:surf-settings-core-client")
 include("surf-settings-example")
