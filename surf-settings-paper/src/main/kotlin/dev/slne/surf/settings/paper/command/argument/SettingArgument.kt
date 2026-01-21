344be638243bf8dc3bc3699e6c93ef64645d0eef
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-21 08:48:54 +0100

    feat: add SettingArgument for enhanced command argument handling in settings API

---
+++ surf-settings-paper/src/main/kotlin/dev/slne/surf/settings/paper/command/argument/SettingArgument.kt
@@ -0,0 +1,52 @@
+package dev.slne.surf.settings.paper.command.argument
+
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.CommandTree
+import dev.jorel.commandapi.arguments.Argument
+import dev.jorel.commandapi.arguments.ArgumentSuggestions
+import dev.jorel.commandapi.arguments.CustomArgument
+import dev.jorel.commandapi.arguments.StringArgument
+import dev.slne.surf.settings.api.setting.Setting
+import dev.slne.surf.settings.core.service.settingsService
+import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
+
+class SettingArgument(nodeName: String) :
+    CustomArgument<Setting, String>(StringArgument(nodeName), { info ->
+        settingsService.getSettingByName(info.input)
+            ?: throw CustomArgumentException.fromAdventureComponent(
+                buildText {
+                    appendPrefix()
+                    error("Die Einstellung wurde nicht gefunden.")
+                })
+    }) {
+    init {
+        this.replaceSuggestions(
+            ArgumentSuggestions.stringCollection {
+                settingsService.settings.map { it.name }
+            }
+        )
+    }
+}
+
+inline fun CommandTree.settingArgument(
+    nodeName: String,
+    optional: Boolean = false,
+    block: Argument<*>.() -> Unit = {}
+): CommandTree = then(
+    SettingArgument(nodeName).setOptional(optional).apply(block)
+)
+
+inline fun Argument<*>.settingArgument(
+    nodeName: String,
+    optional: Boolean = false,
+    block: Argument<*>.() -> Unit = {}
+): Argument<*> = then(
+    SettingArgument(nodeName).setOptional(optional).apply(block)
+)
+
+inline fun CommandAPICommand.settingArgument(
+    nodeName: String,
+    optional: Boolean = false,
+    block: Argument<*>.() -> Unit = {}
+): CommandAPICommand =
+    withArguments(SettingArgument(nodeName).setOptional(optional).apply(block))
