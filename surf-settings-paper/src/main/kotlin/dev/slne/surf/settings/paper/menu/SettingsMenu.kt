e98dc8415806a6cfd7b8b4afa7896ca299dae64d
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-21 09:16:50 +0100

    feat: add placeholder for SettingsMenu implementation with planned categories and settings

---
+++ surf-settings-paper/src/main/kotlin/dev/slne/surf/settings/paper/menu/SettingsMenu.kt
@@ -0,0 +1,17 @@
+package dev.slne.surf.settings.paper.menu
+
+// TODO: Settings Menu
+/**
+ * - Kategorien:
+ * - Chat Settings
+ *     - Chat Pings An/Aus
+ *     - PMs An/Aus
+ * - Lobby Settings
+ *      - Hotbar-Scroll-Sounds An/Aus
+ * - Friend Settings
+ *      - Friend Requests An/Aus
+ *      - Friend Nachjumpen An/Aus
+ * - Clan Settings
+ *      - Clan Einladungen An/Aus
+ *      - Clan Chat Nachrichten An/Aus
+ */
