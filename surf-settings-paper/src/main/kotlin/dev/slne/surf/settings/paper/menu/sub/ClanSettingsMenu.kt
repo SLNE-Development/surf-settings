0ba1b990db766b32f13688aa2116ff2ecc3b68e1
Jo_field <150937665+JoField08@users.noreply.github.com>
2026-02-01 20:34:41 +0100

    fix: hiding additional tooltip for clan invite toggle item

--- surf-settings-paper/src/main/kotlin/dev/slne/surf/settings/paper/menu/sub/ClanSettingsMenu.kt
+++ surf-settings-paper/src/main/kotlin/dev/slne/surf/settings/paper/menu/sub/ClanSettingsMenu.kt
@@ -23,6 +23,8 @@ import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Material
 import org.bukkit.Sound
 import org.bukkit.entity.HumanEntity
+import org.bukkit.inventory.ItemFlag
+import org.bukkit.inventory.meta.FireworkMeta
 
 // TODO: Settings Menu
 /**
@@ -146,6 +148,9 @@ private fun HumanEntity.playClickSound() {
 private fun clanInvitesItem(currentState: Boolean) = buildItem(Material.FIREWORK_ROCKET) {
     displayName {
         localColored("Clan Einladungen".toSmallCaps(), TextDecoration.BOLD)
+    }
+    editMeta(FireworkMeta::class.java) {
+        it.addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP)
     }
 
     buildLore {
